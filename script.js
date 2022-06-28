25d2160738c56748807dda4d87ed4d210e718384
Santhoshini1410 <108294842+Santhoshini1410@users.noreply.github.com>
2022-06-28 16:29:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,97 @@
-let equal_pressed=0;
-//Refer all buttons excluding AC and DEL
-let button_input=document.querySelectorAll(".input-button");
-//Refer input,equal,clear and erase
-let input=document.getElementById("input");
-let equal=document.getElementById("equal");
-let clear=document.getElementById("clear");
-let erase=document.getElementById("erase");
-
-window.onload=() =>{
-    input.value="";
-};
-
-//Access each class using forEach
-button_input.forEach((button_class) => {
-    button_class.addEventListener("click",() => {
-        if(equal_pressed==1) {
-            input.value="";
-            equal_pressed=0;
-        }
-        //display value of each button
-        input.value+=button_class.value;
+const taskInput = document.querySelector(".task-input input"),
+filters = document.querySelectorAll(".filters span"),
+clearAll = document.querySelector(".clear-btn"),
+taskBox = document.querySelector(".task-box");
+let editId,
+isEditTask = false,
+todos = JSON.parse(localStorage.getItem("todo-list"));
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
     });
 });
-
-//Solve the user's input when clicked on equal sign
-equal.addEventListener("click", () => {
-    equal_pressed = 1;
-    let inp_val=input.value;
-    try {
-        //evaluate user's input
-        let solution = eval(inp_val);
-        //True for natural numbers
-        //false for decimals
-        if (Number.isInteger(solution)) {
-            input.value = solution;
+function showTodo(filter) {
+    let liTag = "";
+    if(todos) {
+        todos.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
+        });
+    }
+    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos))
+}
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo()
+});
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask) {
+        if(!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = {name: userTask, status: "pending"};
+            todos.push(taskInfo);
         } else {
-            input.value=solution.toFixed(2);
+            isEditTask = false;
+            todos[editId].name = userTask;
         }
-        } catch (err) {
-            //If user has entered invalid input
-            alert("Invalid Input");
-        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
+    }
 });
-
-//Clear Whole input
-clear.addEventListener("click", () => {
-    input.value= "";
-});
-//Erase Single Digit
-erase.addEventListener("click", () => {
-    input.value = input.value.substr(0, input.value.length - 1);
-});
-
-
-
-
-
